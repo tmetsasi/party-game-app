f5689bfe50aaf1a05ed36faa0334d74f196b1efa
Tomas Metsä-Simola <tmetsasi@Tomass-MacBook-Air.local>
2022-12-29 19:22:05 +0200

    main page init

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to s app! dev branch</Text>
+      <Text>Tää on main page</Text>
       <StatusBar style="auto" />
     </View>
   );
