1cf0bafbcc4fb31e2f10c058c20bd005ce708142
Tomas Metsä-Simola <tmetsasi@Tomass-MacBook-Air.local>
2022-12-29 18:56:24 +0200

    creating main page branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to s app!</Text>
+      <Text>Open up app!</Text>
       <StatusBar style="auto" />
     </View>
   );
